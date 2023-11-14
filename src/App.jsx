72a478fefc21e97b8bfe7fa29ce6d9b8cccf6e04
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-11-14 13:54:11 +0200

    move search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import Search from "./Search";
 import Links from "./Links";
 import Card from "./Card";
 import {
@@ -38,28 +37,29 @@ function App() {
     );
   }
 
+  const animalsFilter = animals
+    .filter((animal) =>
+      animal.name.toLowerCase().startsWith(search.toLowerCase())
+    )
+    .map((animal) => (
+      <Card
+        key={animal.name}
+        title={animal.name}
+        likes={animal.likes}
+        onRemove={() => removeHandler(animal.name)}
+        addLike={() => updateLikes(animal.name, "add")}
+        removeLike={() => updateLikes(animal.name)}
+      />
+    ));
+
   return (
     <>
       <div className="app">
-        <Header />
-        <Search onchange={handleSearch} />
+        <Header onchange={handleSearch} />
         <Links />
         <main>
-          <div className="cards">
-            {animals
-              .filter((animal) =>
-                animal.name.toLowerCase().startsWith(search.toLowerCase())
-              )
-              .map((animal) => (
-                <Card
-                  key={animal.name}
-                  title={animal.name}
-                  likes={animal.likes}
-                  onRemove={() => removeHandler(animal.name)}
-                  addLike={() => updateLikes(animal.name, "add")}
-                  removeLike={() => updateLikes(animal.name)}
-                />
-              ))}
+          <div className="cards match">
+            {animalsFilter.length === 0 ? "No matches!" : animalsFilter}
           </div>
         </main>
         <Footer />
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,10 @@
-const Header = () => {
+import Search from "./Search";
+
+const Header = ({ onchange }) => {
   return (
     <header>
       <h1>Zoo</h1>
+      <Search onchange={onchange} />
     </header>
   );
 };
