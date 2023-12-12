bbd74c7048187fd1b9f3b676ddb9cc790ce66353
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-12-12 16:19:12 +0200

    add redirect file for netlify, fix front page css

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,29 +7,33 @@ import insect from "../images/insect.jpg";
 const Home = () => {
   return (
     <div className="front-page">
-      <div className="left">
-        <Link to="/animals">
-          <img src={animal} alt="animals" />
-          <h2 className="front-page-title">ANIMALS</h2>
-        </Link>
+      <div className="top-row">
+        <div className="link-container">
+          <Link to="/animals">
+            <img src={animal} alt="animals" />
+            <h2 className="front-page-title">ANIMALS</h2>
+          </Link>
+        </div>
+        <div className="link-container">
+          <Link to="/birds">
+            <img src={bird} alt="birds" />
+            <h2 className="front-page-title">BIRDS</h2>
+          </Link>
+        </div>
       </div>
-      <div className="right">
-        <Link to="/birds">
-          <img src={bird} alt="birds" />
-          <h2 className="front-page-title">BIRDS</h2>
-        </Link>
-      </div>
-      <div className="left">
-        <Link to="/fish">
-          <img src={fish} alt="fish" />
-          <h2 className="front-page-title">FISH</h2>
-        </Link>
-      </div>
-      <div className="right">
-        <Link to="/insects">
-          <img src={insect} alt="insects" />
-          <h2 className="front-page-title">INSECTS</h2>
-        </Link>
+      <div className="bottom-row">
+        <div className="link-container">
+          <Link to="/fish">
+            <img src={fish} alt="fish" />
+            <h2 className="front-page-title">FISH</h2>
+          </Link>
+        </div>
+        <div className="link-container">
+          <Link to="/insects">
+            <img src={insect} alt="insects" />
+            <h2 className="front-page-title">INSECTS</h2>
+          </Link>
+        </div>
       </div>
     </div>
   );
