381a73f9688d2ecd3f8b13bed3e00f116d03fe0f
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-12-22 11:30:42 +0200

    add more mobile friendly home page css

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -8,13 +8,13 @@ const Home = () => {
   return (
     <div className="front-page">
       <div className="top-row">
-        <div className="link-container">
+        <div className="link-container animals">
           <Link to="/animals">
             <img src={animal} alt="animals" />
             <h2 className="front-page-title">ANIMALS</h2>
           </Link>
         </div>
-        <div className="link-container">
+        <div className="link-container birds">
           <Link to="/birds">
             <img src={bird} alt="birds" />
             <h2 className="front-page-title">BIRDS</h2>
@@ -22,13 +22,13 @@ const Home = () => {
         </div>
       </div>
       <div className="bottom-row">
-        <div className="link-container">
+        <div className="link-container fish">
           <Link to="/fish">
             <img src={fish} alt="fish" />
             <h2 className="front-page-title">FISH</h2>
           </Link>
         </div>
-        <div className="link-container">
+        <div className="link-container insects">
           <Link to="/insects">
             <img src={insect} alt="insects" />
             <h2 className="front-page-title">INSECTS</h2>
